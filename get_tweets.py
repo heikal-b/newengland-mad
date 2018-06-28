8741fd5656ab373deea6d5004d9fcb775795b56e
heikal-b <heikal93@gmail.com>
2018-06-28 14:43:35 -0700

    Progress

--- get_tweets.py
+++ get_tweets.py
@@ -1,22 +1,40 @@
+# New England Mad: a function for obtaining tweets
+# Author: Heikal Badrulhisham <heikal93@gmail.com>
+# Year: 2018
+"""
+Define the get_tweets() function for loading and saving tweets through Twitter's search page in a browser. The method
+opens a twitter search page with a search query and scrolls down the page repeatedly to load more tweets until there are
+no more tweets to load. This is to circumvent the limitations (in terms of time span and number of requests) of the free
+Twitter API. The main function here collects tweets
+"""
 from selenium import webdriver
+from selenium.common.exceptions import *
 import time
 import csv
 import os
 
 
 def scroll_down(browser, interval=2, second_chance_interval=5):
+    """
+    Helper function for scrolling down a page (for get_tweets())
+    :param browser: Webdriver browser instance to scroll down in
+    :param interval: number of seconds to wait while loading the page
+    :param second_chance_interval: number of seconds to wait for the second time
+    :return:
+    """
     # For checking if it's possible to scroll further
     page = browser.find_element_by_id('page-container')
     win_height = page.size['height']
     reached_bottom = False
 
+    # Scroll down the page
     while not reached_bottom:
         browser.execute_script('window.scrollTo(0,document.body.scrollHeight)')
         time.sleep(interval)
 
+        # If the page is not growing, wait a second time
         new_win_height = page.size['height']
 
-        # If the page is not growing, wait a second time
         if new_win_height == win_height:
             time.sleep(second_chance_interval)
             if win_height == page.size['height']:
@@ -27,7 +45,12 @@ def scroll_down(browser, interval=2, second_chance_interval=5):
 
 def get_tweets(url, save_dir, file_stem='tweets', interval=2, second_chance_interval=10):
     """
-    Get tweets by scrolling down a Twitter search page
+    Get tweets by scrolling down a Twitter search page and save tweets in a CSV file
+    :param url: the search URl
+    :param save_dir: the directory for the saved data file
+    :param file_stem: the stem of the file name for saving tweets
+    :param interval: number of seconds to wait while loading the page
+    :param second_chance_interval: number of seconds to wait for the second time
     :return:
     """
     # Open search page in a browser
@@ -35,9 +58,12 @@ def get_tweets(url, save_dir, file_stem='tweets', interval=2, second_chance_inte
     browser.get(url)
     browser.maximize_window()
 
-    # Dismiss login element
-    page_body = browser.find_element_by_class_name('container')
-    page_body.click()
+    # Dismiss login element if it exists, otherwise move on
+    try:
+        page_body = browser.find_element_by_class_name('container')
+        page_body.click()
+    except NoSuchElementException:
+        pass
 
     # Scroll down the page and wait for new tweets to load
     scroll_down(browser, interval, second_chance_interval)
@@ -54,9 +80,10 @@ def get_tweets(url, save_dir, file_stem='tweets', interval=2, second_chance_inte
 
     # Fin
     browser.close()
+    browser.quit()
 
 
-def main():
+if __name__ == '__main__':
     # url template for Twitter's search age
     url_template = 'https://twitter.com/search?l=en&q=mad%20near%3A%22{0}%2C%20USA%22%20within%3A15mi&src=typd'
 
@@ -73,7 +100,4 @@ def main():
     # get_tweets(url_template.format('Rhode Island'), save_folder, 'rhodeisland')
     # get_tweets(url_template.format('Connecticut'), save_folder, 'test')
 
-
-if __name__ == '__main__':
-    main()
     exit(0)
--- get_tweets_cities.py
+++ get_tweets_cities.py
@@ -1,9 +1,16 @@
+# New England Mad: get tweets by New England cities
+# Author: Heikal Badrulhisham <heikal93@gmail.com>
+# Year: 2018
+"""
+Load tweets from New England cities through a browser and save them in a CSV file
+"""
+
 import csv
 import get_tweets
 import os
 
 
-def main():
+if __name__ == '__main__':
     # Get a list of cities
     cities_csv = csv.reader(open('neweng_cities.csv'))
     cities = [(r[0], r[1]) for r in cities_csv]
@@ -24,15 +31,12 @@ def main():
 
         # Skip if tweets have been collected for the city
         if os.path.exists('{0}/{1}.csv'.format(save_dir, file_name_stem)):
-            break
+            continue
 
         # Location term for searching (City + State abbreviation)
         location = '{0}, {1}'.format(city[0], city[1])
         curr_url = url_template.format(location)
 
-        get_tweets.get_tweets(curr_url, file_name_stem)
+        get_tweets.get_tweets(curr_url, save_dir, file_name_stem, 2, 2)
 
-
-if __name__ == '__main__':
-    main()
     exit(0)
